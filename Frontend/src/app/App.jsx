917dc4144d3303218c9439a90464269cee6b2e52
anikxz <anik.eee.aust@gmail.com>
2026-03-29 15:40:12 +0600

    setup YJS for CRDT

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,31 +1,63 @@
 import "./App.css"
 import { Editor } from "@monaco-editor/react"
 import { MonacoBinding } from "y-monaco"
-import { useRef, useMemo } from "react"
+import { useRef, useMemo, useState } from "react"
 import * as Y from "yjs"
-import { WebsocketProvider } from "y-websocket"
+import { SocketIOProvider } from "y-socket.io"
 
 
 function App() {
   const editorRef = useRef(null)
+  const [ username, setUsername ] = useState(() => {
+    return new URLSearchParams(window.location.search).get("username") || ""
+  })
 
   const ydoc = useMemo(() => new Y.Doc(), [])
   const ytext = useMemo(() => ydoc.getText("monaco"), [ydoc])
 
   const handlemount = (editor) => {
     editorRef.current = editor
-    const provider = new SockerIOProvider("https://localhost:3000", "monaco", ydoc, {
-      autoConnect:true,
-     })
+    const provider = new SocketIOProvider("http://localhost:3000", "monaco", ydoc, {
+      autoConnect: true,
+    })
     const monacoBinding = new MonacoBinding(
-      ytext, 
-      editorRef.current.getModel(), 
-      new Set([editorRef.current]), 
+      ytext,
+      editorRef.current.getModel(),
+      new Set([editorRef.current]),
       provider.awareness
     )
   }
 
 
+  const handlejoin = (e) => {
+    e.preventDefault()
+
+    setUsername(e.target.username.value)
+    window.history.pushState({}, "", "?username=" + e.target.username.value)
+  }
+
+
+
+    if (!username) {
+      return (
+        <main className="h-screen w-full bg-gray-950 flex gap-4 p-4 items-center justify-center">
+          <form onSubmit={handlejoin} className="flex flex-col gap-4">
+            <input
+              type="text"
+              placeholder="Enter your username"
+              className="p-2 rounded-lg bg-gray-800 text-white"
+              name="username"
+            />
+            <button
+              className="p-2 rounded-lg bg-amber-50 text-gray-950 font-bold"
+              type="submit"
+            >
+              Join
+            </button>
+          </form>
+        </main>
+      )
+    }
   
 
   return (
